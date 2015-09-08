775a96fcfceb166729030c495f482e3d840f39cd
Charles Williams <cwilli34@nd.edu>
2015-09-07 22:06:14 -0500

    Updated GitHub username

--- app/views.py
+++ app/views.py
@@ -12,16 +12,16 @@ YOUR_INFO = {
     'bio' : 'While I have been dabbling with front-end development since 2005, \
              I just graduated from Indiana University last year with a B.S. in \
              Informatics, along with a cognate in English.  I have experience \
-             with C#, Microsoft Access development & runtime, Micrsoft SQL, \
+             with C#, Microsoft Access development & runtime, Microsoft SQL, \
              .NET, PHP, JavaScript, HTML, CSS, jQuery, AJAX, and other front- \
              end related frameworks and technologies.  I just started working \
              for the University of Notre Dame this year as a Research Programmer.  \
              Django is our go to framework for web development with PostgreSQL, \
-             but we also dabble with PHP, Nodejs, MongoDB, Mean.js, AngularJS, \
+             but we also use PHP, Nodejs, MongoDB, Mean.js, AngularJS, \
              and many others.',
     'email' : 'cwilli34@nd.edu', # Leave blank if you'd prefer not to share your email with other conference attendees
     'twitter_username' : 'charlwillia6', # No @ symbol, just the handle.
-    'github_username' : "charlwillia6",
+    'github_username' : "cwilli34",
     'headshot_url' : 'https://avatars3.githubusercontent.com/u/9610352?v=3&s=460', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
 
